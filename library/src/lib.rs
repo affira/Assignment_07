10d244dc26084fab7c304ccbc2cdbc358f053159
feera02 <rabbiamughal4@gmail.com>
2020-01-15 12:15:52 +0500

    Question 3

---
+++ library/src/lib.rs
@@ -0,0 +1,21 @@
+
+pub mod movie {
+ 
+    pub mod genre {
+        pub fn sci_fi()
+        {
+            println!("***SCIENCE FICTION MOVIES***");
+            println!("1. ALITA: THE BATTLE ANGEL");
+            println!("2. STAR WARS:THE RISE OF SKYWALKER\n");
+        }
+
+        pub fn actn()
+        {
+            println!("***ACTION MOVIES***");
+            println!("1. GODZILLA: KING OF MONSTERS");
+            println!("2. SHAZAM!\n");
+        }
+
+    }
+   
+}
